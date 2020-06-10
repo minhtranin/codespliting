1ae32a88ffa60c6121b09defc40657b69ed33c6b
MinhTran <minhtran.in@outlook.com>
2020-06-10 22:24:36 +0700

    split component

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,12 @@ import reducer from './reducers/redux';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import saga from 'redux-saga';
-import rootSaga from './sagas/saga';
-const sagaMiddleware = saga();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+// import rootSaga from './sagas/saga';
+// const sagaMiddleware = saga();
+const store = createStore(reducer)//, applyMiddleware(sagaMiddleware));
 const root = document.getElementById('root');
 
 ReactDOM.render(<Provider store={store}>
     <App/>
 </Provider>, root);
-sagaMiddleware.run(rootSaga);
+//sagaMiddleware.run(rootSaga);
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import Admin from '../../pages/Admin/index'
+//import Admin from '../../pages/Admin/index'
+import loadable from '@loadable/component';
+const OtherComponent = loadable(() => import('../../pages/Admin/index'), {
+    fallback: <div>Loading...</div>,
+  });
 import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 class App extends React.Component{
     render(){
+        const products = {
+            product: {
+                code: "1a",
+                name: "bao viet"
+            }
+        }
         return (
             <Router>
             <div className="App">
-                <Route path="/about" component={Admin} />
+                <Route path="/about" component={OtherComponent} />
             </div>
         </Router>
         )
